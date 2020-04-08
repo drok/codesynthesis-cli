720c5a33b6a49cf328fdd7611f49153cf8f60247
Karen Arutyunov <karen@codesynthesis.com>
2020-04-08 14:51:57 +0300

    Separate tests and examples into individual packages
    Also make cli module to be explicitly enabled via the config.cli configuration
    variable.

---
+++ cli-tests/ctor/driver.cxx
@@ -0,0 +1,60 @@
+// file      : ctor/driver.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// license   : MIT; see accompanying LICENSE file
+
+#include "test.hxx"
+
+int
+main (int argc, char* argv[])
+{
+  // Test that we can call all the c-tors unambiguously.
+  //
+  {
+    options o1 (argc, argv);
+    options o2 (argc, argv, true);
+    options o3 (argc, argv, true,
+                cli::unknown_mode::fail);
+    options o4 (argc, argv, true,
+                cli::unknown_mode::fail,
+                cli::unknown_mode::stop);
+  }
+
+  {
+    options o1 (1, argc, argv);
+    options o2 (1, argc, argv, true);
+    options o3 (1, argc, argv, true,
+                cli::unknown_mode::fail);
+    options o4 (1, argc, argv, true,
+                cli::unknown_mode::fail,
+                cli::unknown_mode::stop);
+  }
+
+  {
+    int end;
+    options o1 (argc, argv, end);
+    options o2 (argc, argv, end, true);
+    options o3 (argc, argv, end, true,
+                cli::unknown_mode::fail);
+    options o4 (argc, argv, end, true,
+                cli::unknown_mode::fail,
+                cli::unknown_mode::stop);
+  }
+
+  {
+    int end;
+    options o1 (1, argc, argv, end);
+    options o2 (1, argc, argv, end, true);
+    options o3 (1, argc, argv, end, true,
+                cli::unknown_mode::fail);
+    options o4 (1, argc, argv, end, true,
+                cli::unknown_mode::fail,
+                cli::unknown_mode::stop);
+  }
+
+  {
+    cli::argv_scanner s (argc, argv);
+    options o1 (s);
+    options o2 (s, cli::unknown_mode::fail);
+    options o3 (s, cli::unknown_mode::fail, cli::unknown_mode::stop);
+  }
+}
---
+++ cli-tests/file/driver.cxx
@@ -0,0 +1,39 @@
+// file      : file/driver.cxx
+// author    : Boris Kolpackov <boris@codesynthesis.com>
+// license   : MIT; see accompanying LICENSE file
+
+// Test argv_file_scanner.
+//
+#include <memory>
+#include <string>
+#include <iostream>
+
+#include "test.hxx"
+
+using namespace std;
+
+int
+main (int argc, char* argv[])
+{
+  try
+  {
+    string a (argc > 1 ? argv[1] : "");
+
+    // Special modes.
+    //
+    // ---- <file>
+    // --- <file>
+    //
+    unique_ptr<cli::scanner> s (
+      a == "----" ? new cli::argv_file_scanner (argv[2], "--file") :
+      a == "---"  ? new cli::argv_file_scanner (argv[2])           :
+      new cli::argv_file_scanner (argc, argv, "--file"));
+
+    while (s->more ())
+      cout << s->next () << endl;
+  }
+  catch (const cli::exception& e)
+  {
+    cerr << e << endl;
+  }
+}
